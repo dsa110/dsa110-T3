ad632909392702bd5d1c12d7e701d08d6927cc00
Dana Simard <dana.simard@astro.caltech.edu>
2022-04-18 08:41:05 -0700

    Add rsync_file

--- dsaT3/utils.py
+++ dsaT3/utils.py
@@ -83,3 +83,31 @@ def archive(datestring, T3root='/media/ubuntu/data/dsa110/T3/'):
             
     return saved_trigname        
 
+
+def rsync_file(infile: str, outfile: str) -> str:
+    """Rsyncs a file from the correlator machines.
+
+    Parameters
+    ----------
+    infile : str
+        The sourcefile string, e.g. 'corr01.sas.pvt:/home/user/data/fl_out.1.5618974'
+    outfile : str
+        The destination string, e.g. '/home/user/data/fl_out.1.5618974'
+
+    Returns
+    -------
+    str
+        The full path to the rsynced file in its destination.
+    """
+    command = f"rsync -avvP --inplace {infile} {outfile}"
+    process = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        shell=True
+    )
+    proc_stdout = str(process.communicate()[0].strip())
+    print(proc_stdout)
+    if process.returncode != 0:
+        return None
+    return outfile
