a6a45f408bc6001ccaeb0b267951a210b1b13ae0
ubuntu <ubuntu@lxd110h23>
2022-05-19 22:24:22 +0000

    Remove extra definition of rsync_file

--- dsaT3/utils.py
+++ dsaT3/utils.py
@@ -43,34 +43,6 @@ def check_voltages(candname):
 
     writefile(dd, filename)
 
-def rsync_file(infile, outfile):
-    """Rsyncs a file from the correlator machines.
-
-    Parameters
-    ----------
-    infile : str
-        The sourcefile string, e.g. 'corr01.sas.pvt:/home/user/data/fl_out.1.5618974'
-    outfile : str
-        The destination string, e.g. '/home/user/data/fl_out.1.5618974'
-
-    Returns
-    -------
-    str
-        The full path to the rsynced file in its destination.
-    """
-    command = 'rsync -avvP --inplace {0} {1}'.format(infile, outfile)
-    process = subprocess.Popen(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        shell=True
-    )
-    proc_stdout = str(process.communicate()[0].strip())
-    print(proc_stdout)
-    if process.returncode != 0:
-        return None
-    return outfile
-
     
 def archive(datestring, T3root='/media/ubuntu/data/dsa110/T3/'):
     """ Archive data from corr nodes to local (h23).
