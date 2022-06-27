75aa510f84553bbe975693488506fb003003fc88
Dana Simard <dana.simard@astro.caltech.edu>
2022-06-27 13:11:08 -0700

    Add hardlinking for T2, hdf5 files

--- dsaT3/data_manager.py
+++ dsaT3/data_manager.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 from copy import deepcopy
+from itertools import chain
+from astropy.time import Time
 from dsautils import dsa_syslogger as dsl
 from dsautils import cnf
 
@@ -36,6 +38,11 @@ class DataManager:
             {
                 'target': "{operations_dir}/correlator/{hdf5_name}*.hdf5",
                 'destination': "{candidates_dir}/{candname}/Level3/"
+            },
+        'T2_csv':
+            {
+                'target': "{operations_dir}/T2/cluster_output.csv",
+                'destination': "{candidates_dir}/{candname}/Level2/T2_{candname}.csv"
             }
     }
 
@@ -108,3 +115,50 @@ class DataManager:
         if self.nbeams:     
             self.candparams['filterbank'] = dest_file.parent
             self.logger.info("Filterbank linked.")
+
+    def link_beamformer_weights(self):
+        pass
+
+    def link_hdf5_files(self, hours_to_save: int = 2):
+        candtime = Time(self.candparams['mjds'], 
+        format='mjd', precision=0)
+        start = candtime - hours_to_save / 2 * u.h
+        stop = candtime + hours_to_save / 2 * u.h
+        
+        self.logger.info(f"Linking HDF5 files for {hours_to_save} hours.")
+        
+        
+
+    def link_field_ms(self):
+        pass
+
+    def link_caltables(self):
+        pass
+
+    def link_T2_csv(self):
+        """Link T2 csv to candidate directory."""
+
+        self.logger.info("Linking T2 csv to candidate directory.")
+
+        source_file = Path(
+            self.directory_structure['T2_csv']['target'].format(
+                operations_dir=self.operations_directory))
+        dest_file = Path(
+            self.directory_structure['T2_csv']['destination'].format(
+                candidates_dir=self.candidates_dir, candname=self.candname))
+        self.logger.info(f"Linking {source_file} to {dest_file}")
+        dest_file.link_to(source_file)
+        self.candparams['T2_csv'] = str(dest_file)
+        self.logger.info("T2 csv linked.")
+
+
+def within_times(start_time, end_time, time):
+    """Check if time is within start and end times."""
+    return start_time <= time <= end_time
+
+
+def time_from_hdf5_filename(filepath):
+    """Get time from hdf5 file name."""
+    return Time(filepath.stem.split('_')[0])
+
+
