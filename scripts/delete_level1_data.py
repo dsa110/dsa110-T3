21838c67b55f94f562462c944381caa8477139fa
dsa <vikram@caltech.edu>
2022-07-06 00:48:27 +0000

    fix bug comparing datetimes

--- scripts/delete_level1_data.py
+++ scripts/delete_level1_data.py
@@ -1,21 +1,27 @@
 from pathlib import Path
 import datetime
+import time
 
 operations_dir = Path("/dataz/dsa110/operations")
 subdirs_to_clear = [
     ("correlator", "*.hdf5"),
     ("T1/beams", "*.dat"),
-    ("T1", "*/*.cand"),
-    ("T2/cluster_output", "*.cand")
-    ("T2/cluster_output", "*.json")]
+    ("T1", "*/*.fil"),
+    ("T2/cluster_output", "*.cand"),
+    ("T2/cluster_output", "*.json"),
+]
 
 cutoff = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=7)
 print(
     f"Removing operation files last modified prior to "
-    f"{cutoff.strftime('%Y-%m-%d')}")
+    f"{cutoff.strftime('%Y-%m-%dT%H:%M:%S')} UTC")
 
 for subdir, pattern in subdirs_to_clear:
     for file in (operations_dir / subdir).glob(pattern):
-        if datetime.datetime.fromtimestamp(file.stat().st_mtime) < cutoff:
+        modtime = datetime.datetime.fromtimestamp(file.stat().st_mtime)
+        # modtime is timezone naive, so we set it to utc
+        # lxc managed containers are all using utc
+        modtime = modtime.replace(tzinfo=cutoff.tzinfo)
+        if modtime < cutoff:
             print(f'Removing {file}')
             # file.unlink()
