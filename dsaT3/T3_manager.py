81c11d70c00557a25b37ec946e0f0f8097906880
ubuntu <ubuntu@lxd110h23>
2025-02-27 20:21:07 +0000

    will not run fast_response if ew beam = ns beam

--- dsaT3/T3_manager.py
+++ dsaT3/T3_manager.py
@@ -100,19 +100,13 @@ def run_filplot(d, wait=False, lock=None):
         #                        rficlean=False, ndm=1, nfreq_plot=32, save_data=False,
         #                        fllisting=None)
 
-        # assuming inj8 DMs are untrustworthy. inj10 is always found as too wide (ibox=16)
-        #if d.ibeam_prob > 0.95 and d.ibox < 16 and d.snr > 13 and not (d.dm > 624 and d.dm < 628):
-        #    print('Running fast_response')
-        #    fast_response(d)
-        #else:
-        #    print('Not running fast_response. Event is too wide/weak/low-ibeam_prob/injected-dm.')
-
         d.candplot, d.probability, d.real = filf.filplot_entry(asdict(d), rficlean=False, classify=True)
-        if d.probability > 0.95 and d.ibox < 16 and d.snr > 13 and d.real:
+        if d.probability > 0.95 and d.ibox < 16 and d.snr > 11 and d.real and d.beams0 % 256 != d.beams1 % 256:
+            # all injections lie on ns beam = ew beam (mod 256), so we are suspicious of those
             print("Running fast_response")
             fast_response(d)
         else:
-            print('Not running fast_response. Event is too wide/weak/low-ibeam_prob')
+            print('Not running fast_response. Event is too wide/weak/low-ibeam_prob/injection')
 
     except Exception as exception:
         logging_string = "Could not make filplot {0} due to {1}.  Callback:\n{2}".format(
