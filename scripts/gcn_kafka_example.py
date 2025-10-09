a74982c5ecc338099d0583cb5343223ce30099f6
ubuntu <ubuntu@lxd110h23>
2025-10-09 23:50:54 +0000

    gcn consumer/producer working with test gcn credentials

--- scripts/gcn_kafka_example.py
+++ scripts/gcn_kafka_example.py
@@ -1,15 +1,10 @@
+#! /usr/bin/env
+
 from gcn_kafka import Consumer, Producer
 import argparse
 import json
 import sys
 import os
-
-# Connect as a consumer (client "dsa110")
-# Warning: don't share the client secret with others.
-client_id_test = os.environ.get('GCN_ID_TEST', '')
-client_secret_test = os.environ.get('GCN_SECRET_TEST', '')
-client_id_prod = os.environ.get('GCN_ID', '')
-client_secret_prod = os.environ.get('GCN_SECRET', '')
 
 if __name__ == "__main__":
     # Parse command line arguments to determine if running as consumer or producer and environment
@@ -18,42 +13,45 @@ if __name__ == "__main__":
         sys.exit(1)
     else:
         parser = argparse.ArgumentParser(description="Run as consumer or producer.")
-        parser.add_argument('-c', '--asconsumer', action='store_true', help='Run as consumer')
-        parser.add_argument('-p', '--asproducer', action='store_true', help='Run as producer')
+        parser.add_argument('-m', '--mode', choices=['consumer', 'producer'], help='Run in consumer or producer mode')
         parser.add_argument('-e', '--env', choices=['prod', 'test'], default='test', help='Environment: prod or test (default: test)')
+        parser.add_argument('-t', '--topic', default='gcn.notices.dsa110.frb', help='Kafka topic to use')
         args, unknown = parser.parse_known_args()
 
-        if not args.asconsumer and not args.asproducer:
-            print("Please specify either -c (consumer) or -p (producer).")
-            sys.exit(1)
 if args.env == 'test':
-    client_id = client_id_test
-    client_secret = client_secret_test
+    domain = 'test.gcn.nasa.gov'
+    if args.mode == "consumer":
+        client_id = os.environ.get('GCN_ID_CON_TEST', '')
+        client_secret = os.environ.get('GCN_SECRET_CON_TEST', '')
+    if args.mode == "producer":
+        client_id = os.environ.get('GCN_ID_PRO_TEST', '')
+        client_secret = os.environ.get('GCN_SECRET_PRO_TEST', '')
 else:
-    client_id = client_id_prod
-    client_secret = client_secret_prod
+    domain = 'gcn.nasa.gov'
+    print("Still need to get credentials for production mode")
+    sys.exit(1)
 
-if args.asproducer:
+if args.mode == "producer":
+    print(client_id, domain, args.topic)
     # Connect as a producer (client "dsa110")
-    producer = Producer(client_id=client_id, client_secret=client_secret)
-
-    # Produce a message to the topic
-    topic = 'gcn.notices.dsa110'
+    producer = Producer(client_id=client_id, client_secret=client_secret, domain=domain)
 
     # JSON data converted to byte string format
     data = json.dumps({
-        '$schema': 'https://gcn.nasa.gov/schema/vX.Y.Z/gcn/notices/mission/SchemaName.schema.json',
+        '$schema': 'https://gcn.nasa.gov/schema/v6.0.0/gcn/notices/mission/SchemaName.schema.json',
         'key': 'value'
-    }).encode()
-    producer.produce(topic, data)
-    producer.flush()
+    }).encode("utf-8")
 
-if args.asconsumer:
+    producer.produce(args.topic, data)
+    producer.flush()
+elif args.mode == 'consumer':
     # Connect as a consumer
-    consumer = Consumer(client_id=client_id, client_secret=client_secret)
+    config = {'auto.offset.reset': 'earliest'}
+    consumer = Consumer(client_id=client_id, client_secret=client_secret, domain=domain, config=config)
 
     # Subscribe to topics and receive alerts
-    consumer.subscribe(['igwn.gwalert'])
+    consumer.subscribe([args.topic])
+
     while True:
         for message in consumer.consume(timeout=1):
             if message.error():
